16cde42dc73b81b229c0b353bad15a0e0619c883
Mary Rossi <maryrossi106@gmail.com>
2022-09-18 21:41:55 -0500

    added models & index route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const budget = require('./models/budget.js')
+let bankAccount = 0
 
 // Middleware
 app.use(express.urlencoded({extended: false}))
@@ -10,6 +12,13 @@ app.use(express.urlencoded({extended: false}))
 app.get('/', (req, res) => {
   res.send('Welcome!')
 });
+
+// Index
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', {
+        budget, bankAccount,
+    });
+  });
 
 // Listening
 app.listen(port, () => console.log(`Express is listening on Port: ${port}`))
